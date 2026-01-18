4b20e04cae4ad156548e96aa8b628697fa577ea3
Khalifa Mohammad <36789450+khalifamohammad@users.noreply.github.com>
2026-01-18 20:52:00 +0200

    Update animations.cpp
    vertex shader and tree animation

--- animations.cpp
+++ animations.cpp
@@ -11,3 +11,44 @@
       for (const auto& pos : treePositions) {
           DrawAnimatedMesh(treeModel, pos, glm::vec3(3.0f), true);
       }
+
+-------------------------
+vertex shader update
+      #version 400
+
+layout (location = 0) in vec3 pos;
+layout (location = 1) in vec3 normals;
+layout (location = 2) in vec2 texCoord;
+
+out vec2 TexCoords; 
+out vec3 Normal; 
+out vec3 FragPos; 
+
+uniform mat4 MVP;
+uniform mat4 model;
+uniform float time;
+uniform bool isTree;
+
+void main()
+{
+    TexCoords = texCoord;
+    vec3 animatedPos = pos;
+
+    if (isTree) {
+        float height = max(0.0, pos.y + 4.0); 
+        float mask = pow(height, 2.0) * 0.01; 
+
+        float swaySpeed = 1.2;
+        float swayStrength = 0.2;
+
+        animatedPos.x += sin(time * swaySpeed + (pos.y * 0.5)) * mask * swayStrength;
+        animatedPos.z += cos(time * swaySpeed * 0.8 + (pos.y * 0.5)) * mask * (swayStrength * 0.5);
+    }
+    
+    FragPos = vec3(model * vec4(animatedPos, 1.0f));
+    Normal = mat3(transpose(inverse(model))) * normals;
+    gl_Position = MVP * vec4(animatedPos, 1.0f);
+}
+
+
+
